ffff77392b85c9f235bc59ace0ad85236afb0832
Denis <aie-dinis2007@yandex.ru>
2021-05-31 17:31:17 +0300

    Initial Commit

---
+++ proba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proba
+//
+//  Created by Денис Александров on 31.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
